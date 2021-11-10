22fbdc581771b46b2f8f6811aa3171b679f6e429
willmmoses <36004763+willmmoses@users.noreply.github.com>
2021-11-10 13:22:13 -0600

    Commented, cleaned up, and functional.

--- pyMessage.py
+++ pyMessage.py
@@ -1,15 +1,28 @@
 import sqlite3
 from collections import Counter
-
 import pandas as pd
+
+"""
+A library for loading an iMessage database into a Pandas dataframe. Not particularly performant, but it works.
+"""
 
 
 def db_connector(filepath):
+    """
+    Creates an SQLite server from the given filepath
+    :param filepath: the location of the SQLite database
+    :return: An SQLite connection
+    """
     print("Initiating connection to database")
     return sqlite3.connect(filepath)
 
 
 def db_reader(conn):
+    """
+    Reads all information from the given SQLite database and returns it as a series of dataframes
+    :param conn: SQLite connection
+    :return: dataframes of tables
+    """
     print("Reading messages")
     message_df = pd.read_sql('SELECT * FROM message', conn)
     print("Total messages read:", message_df.shape[0])
@@ -33,97 +46,94 @@ def db_reader(conn):
     return message_df, chat_df, handle_df, attachment_df, chat_handle_df, chat_message_df, message_attachment_df
 
 
-def message_cleaner(df):
-    clean_message = df.loc[:, ['ROWID', 'guid', 'text', 'handle_id', 'date', 'date_read', 'date_delivered',
-                               'associated_message_guid']].rename(
-        columns={'ROWID': 'message_id', 'guid': 'message_guid', 'ck_sync_state': 'message_ck_sync_state',
-                 'sr_ck_sync_state': 'message_', 'country': 'message_country', 'service': 'message_service',
-                 'ck_record_id': 'message_ck_record_id', 'sr_ck_record_id': 'message_sr_ck_record_id'})
-    return clean_message
-
-
-def chat_cleaner(df):
-    clean_chat = df.loc[:, ['ROWID', 'guid', 'chat_identifier', 'display_name']].rename(
-        columns={'ROWID': 'chat_id', 'guid': 'chat_guid'})
-    return clean_chat
-
-
-def handle_cleaner(df):
-    """
-    Renames conflicting column names to allow for safe merging with other frames.
-    :param df: handle dataframe
-    :return: cleaned dataframe.
-    """
-    clean_handle = df.rename(
-        columns={'ROWID': 'handle_id', 'id': 'sender', 'country': 'handle_country', 'service': 'handle_service'})
-    return clean_handle
-
-
-def attachment_cleaner(df):
-    """
-    Returns a slimmed-down version of the attachment dataframe.
-    :param df: the attachment dataframe
-    :return: a cleaned version of the dataframe
-    """
-    clean_attachment = df.loc[:, [
-                                     'ROWID', 'guid', 'created_date', 'filename', 'uti', 'mime_type', 'transfer_name',
-                                     'total_bytes', 'is_sticker']].rename(
-        columns={'ROWID': 'attachment_id', 'guid': 'attachment_guid'})
-    return clean_attachment
-
-
-def chat_handle_merger(chat_df, handle_df, join_df):
-    print('Merging chat and handle tables')
-    merged_df = join_df.join(handle_df.set_index('handle_ROWID'), on='handle_id')
-    merged_df = merged_df.join(chat_df.set_index('chat_ROWID'), on='chat_id')
-    print(merged_df.dtypes)
-    print('Merge complete')
-    return merged_df
-
-
-def conflict_renamer(df, conflicts, table_name):
-    replacement = table_name + '_id'
-    deconflicted = df.rename(columns={'ROWID': replacement})
-    for name in conflicts:
-        if name in df.columns.tolist():
-            replacement = table_name + '_' + name
-            deconflicted = deconflicted.rename(columns={name: replacement})
-    return deconflicted
-
-
-def merger(chat_df, message_df, handle_df, attachment_df, chat_message_join_df, message_attachment_join_df):
-    print('Merging chat and message tables')
-    merged_df = chat_message_join_df.join(chat_df.set_index('chat_id'), on='chat_id')
-    merged_df = merged_df.join(message_df.set_index('message_id'), on='message_id')
-    merged_df = merged_df.join(handle_df.set_index('handle_id'), on='handle_id')
-    merged_df = merged_df.join(message_attachment_join_df.set_index('message_id'), on='message_id')
-    merged_df = merged_df.join(attachment_df.set_index('attachment_id'), on='attachment_id')
-    print(merged_df.dtypes)
-    print("Merge complete")
-    return merged_df
-
-
 def column_checker(chat_df, message_df, attachment_df, handle_df):
-    columns = chat_df.columns.tolist() + message_df.columns.tolist() + attachment_df.columns.tolist() + handle_df.columns.tolist()
+    """
+    Checks the columns of the major tables for any conflicting column names
+    :param chat_df: the chat table dataframe
+    :param message_df: the message table dataframe
+    :param attachment_df: the attachment table dataframe
+    :param handle_df: the handle table dataframe
+    :return: a list of conflicting column names
+    """
+    columns = chat_df.columns.tolist() + message_df.columns.tolist() + attachment_df.columns.tolist() + \
+              handle_df.columns.tolist()
     conflicts = [item for item, count in Counter(columns).items() if count > 1]
     return conflicts
 
 
-def main():
-    print("Hello World")
+def conflict_rename(df, conflicts, table_name):
+    """
+    Renames the conflicting columns in the given dataframe by prepending the string passed in table name, followed by an
+    underscore.
+    :param df: the dataframe
+    :param conflicts: the list of conflicting column names
+    :param table_name: the string to prepend to the conflicting columns
+    :return: the conflict-free version of the dataframe
+    """
+    replacement = table_name + '_id'
+    conflict_free = df.rename(columns={'ROWID': replacement})
+    for name in conflicts:
+        if name in df.columns.tolist():
+            replacement = table_name + '_' + name
+            conflict_free = conflict_free.rename(columns={name: replacement})
+    return conflict_free
+
+
+def merger(chat_df, message_df, handle_df, attachment_df, chat_message_join_df, message_attachment_join_df):
+    """
+    Merges the given dataframes to create a single master dataframe containing all information. Assumes all dataframes
+    have had their conflicting columns renamed via conflict_rename
+    :param chat_df: the chat dataframe
+    :param message_df: the message dataframe
+    :param handle_df: the handle dataframe
+    :param attachment_df: the attachment dataframe
+    :param chat_message_join_df: the chat_message_join dataframe
+    :param message_attachment_join_df: the message_attachment_join dataframe
+    :return: the combined dataframe
+    """
+    print('Merging tables')
+    print('Merging chats and messages')
+    merged_df = chat_message_join_df.join(chat_df.set_index('chat_id'), on='chat_id')
+    merged_df = merged_df.join(message_df.set_index('message_id'), on='message_id')
+    print("Merge complete")
+    print("Merging handles")
+    merged_df = merged_df.join(handle_df.set_index('handle_id'), on='handle_id')
+    print('Merge complete')
+    print('Merging attachments')
+    merged_df = merged_df.join(message_attachment_join_df.set_index('message_id'), on='message_id')
+    merged_df = merged_df.join(attachment_df.set_index('attachment_id'), on='attachment_id')
+    print("Merge complete")
+    print("All tables merged")
+    return merged_df
+
+
+def loader(filename):
+    """
+    A tidy wrapper for all of the functions. Returns the master dataframe from a given chat database.
+    :param filename: the file name of the SQLite db
+    :return: the master dataframe
+    """
     message_df, chat_df, handle_df, attachment_df, chat_handle_df, chat_message_df, message_attachment_df = db_reader(
-        db_connector("chat.db"))
+        db_connector(filename))
     conflicts = column_checker(chat_df, message_df, attachment_df, handle_df)
     if conflicts:
-        decon_message = conflict_renamer(message_df, conflicts, 'message')
-        decon_chat = conflict_renamer(chat_df, conflicts, 'chat')
-        decon_attachment = conflict_renamer(attachment_df, conflicts, 'attachment')
-        decon_handle = conflict_renamer(handle_df, conflicts, 'handle')
-        merged = merger(decon_chat, decon_message, decon_handle, decon_attachment, chat_message_df,
+        cf_message = conflict_rename(message_df, conflicts, 'message')
+        cf_chat = conflict_rename(chat_df, conflicts, 'chat')
+        cf_attachment = conflict_rename(attachment_df, conflicts, 'attachment')
+        cf_handle = conflict_rename(handle_df, conflicts, 'handle')
+        merged = merger(cf_chat, cf_message, cf_handle, cf_attachment, chat_message_df,
                         message_attachment_df)
     else:
         merged = merger(chat_df, message_df, handle_df, attachment_df, chat_message_df, message_attachment_df)
-    print(merged.info(max_cols=merged.shape[1]))
+    return merged
+
+
+def main():
+    """
+    Perfunctory main function. Superfluous
+    :return: 0 on clean exit
+    """
+    loader('chat.db')
 
 
 if __name__ == '__main__':
